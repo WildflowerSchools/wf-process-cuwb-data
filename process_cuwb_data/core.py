41a98ad1e9976be8bfc5f731473d1888e5a58807
Theodore Quinn <tcquinn@tcquinn.org>
2020-01-09 20:31:15 -0600

    Add methods for fetching and processing entity assignments

--- process_cuwb_data/core.py
+++ process_cuwb_data/core.py
@@ -37,7 +37,8 @@ def fetch_cuwb_data(
     ])
     df.rename(
         columns = {
-            'timestamp_secondary': 'timestamp'
+            'timestamp_secondary': 'timestamp',
+            'serial_number': 'device_serial_number'
         },
         inplace=True
     )
@@ -54,8 +55,7 @@ def fetch_cuwb_data(
     df.sort_index(inplace=True)
     return df
 
-def add_device_metadata(
-    df,
+def fetch_cuwb_tag_metadata(
     device_type='UWBTAG'
 ):
     client = MinimalHoneycombClient()
@@ -79,6 +79,117 @@ def add_device_metadata(
             ]}
         ]
     )
-    metadata_df = pd.DataFrame(result.get('data'))
-    df = df.join(metadata_df.set_index('serial_number'), on='serial_number')
+    df = pd.DataFrame(result.get('data'))
+    df.rename(
+        columns={
+            'part_number': 'device_part_number',
+            'serial_number': 'device_serial_number',
+            'name': 'device_name',
+            'tag_id': 'device_tag_id',
+            'mac_address': 'device_mac_address'
+        },
+        inplace=True
+    )
     return df
+
+def fetch_entity_assignments(
+    device_ids
+):
+    client = MinimalHoneycombClient()
+    result = client.request(
+        request_type="query",
+        request_name='searchEntityAssignments',
+        arguments={
+            'query': {
+                'type': 'QueryExpression!',
+                'value': {
+                    'field': 'device',
+                    'operator': 'IN',
+                    'values': device_ids
+                }
+            }
+        },
+        return_object = [
+            {'data': [
+                'entity_assignment_id',
+                'entity_type',
+                {'entity': [
+                    {'... on Person': [
+                        'person_id',
+                        'name',
+                        'first_name',
+                        'last_name',
+                        'nickname',
+                        'short_name',
+                        'person_type'
+                    ]},
+                    {'... on Tray': [
+                        'tray_id',
+                        'name',
+                        'part_number',
+                        'serial_number'
+                    ]}
+                ]},
+                {'device': [
+                    'device_id'
+                ]},
+                'start',
+                'end'
+            ]}
+        ]
+    )
+    df = pd.DataFrame(result.get('data'))
+    df['entity_id'] = df['entity'].apply(lambda x: x.get('tray_id', x.get('person_id')))
+    df['entity_name'] = df['entity'].apply(lambda x: x.get('name'))
+    df['entity_first_name'] = df['entity'].apply(lambda x: x.get('first_name'))
+    df['entity_last_name'] = df['entity'].apply(lambda x: x.get('last_name'))
+    df['entity_nickname'] = df['entity'].apply(lambda x: x.get('nickname'))
+    df['entity_short_name'] = df['entity'].apply(lambda x: x.get('short_name'))
+    df['entity_person_type'] = df['entity'].apply(lambda x: x.get('person_type'))
+    df['entity_part_number'] = df['entity'].apply(lambda x: x.get('part_number'))
+    df['entity_serial_number'] = df['entity'].apply(lambda x: x.get('serial_number'))
+    df['device_id'] = df['device'].apply(lambda x: x.get('device_id'))
+    df['start'] = pd.to_datetime(df['start'])
+    df['end'] = pd.to_datetime(df['end'])
+    df = df.reindex(columns=[
+        'entity_assignment_id',
+        'device_id',
+        'entity_id',
+        'start',
+        'end',
+        'entity_type',
+        'entity_name',
+        'entity_first_name',
+        'entity_last_name',
+        'entity_nickname',
+        'entity_short_name',
+        'entity_person_type',
+        'entity_part_number',
+        'entity_serial_number'
+    ])
+    return df
+
+def lookup_assignment(
+    assignments_df,
+    lookup_value,
+    timestamp,
+    lookup_value_field_name,
+    assignment_id_field_name
+):
+    assignment_ids=[]
+    for index, row in assignments_df.iterrows():
+        if row[lookup_value_field_name] != lookup_value:
+            continue
+        if row['start'] > timestamp:
+            continue
+        if row['end'] < timestamp:
+            continue
+        assignment_ids.append(row[assignment_id_field_name])
+    if len(assignment_ids) > 1:
+        raise ValueError('More than one assignment found for obect {} at timestamp {}'.format(
+            lookup_value,
+            timestamp
+        ))
+    if len(assignment_ids) == 0:
+        return None
+    return assignment_ids[0]
