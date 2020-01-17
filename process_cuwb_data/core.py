e3336dff60a2fe2a26135f4016ae2cb177a391c2
Theodore Quinn <tcquinn@tcquinn.org>
2020-01-17 13:41:16 -0600

    Add wrappers for fetching position, accelerometer, status data

--- process_cuwb_data/core.py
+++ process_cuwb_data/core.py
@@ -117,54 +117,92 @@ def fetch_cuwb_position_data(
     start_time,
     end_time,
     read_chunk_size=2,
-    object_type_honeycomb='DEVICE',
-    object_id_field_name_honeycomb='device_type',
-    object_ids=['UWBTAG']
+    device_type='UWBTAG'
 ):
-    dbc = DatabaseConnectionHoneycomb(
-        environment_name_honeycomb = environment_name,
-        time_series_database = True,
-        object_database = True,
-        object_type_honeycomb = object_type_honeycomb,
-        object_id_field_name_honeycomb = object_id_field_name_honeycomb,
-        read_chunk_size=read_chunk_size
+    df = fetch_cuwb_data(
+        environment_name,
+        start_time,
+        end_time,
+        read_chunk_size,
+        device_type
     )
-    data = dbc.fetch_data_object_time_series(
-        start_time = start_time,
-        end_time = end_time,
-        object_ids = object_ids
-    )
-    df = pd.DataFrame(data)
     df = df.loc[df['type'] == 'position'].copy()
-    df = df.reindex(columns=[
-        'serial_number',
-        'timestamp_secondary',
-        'x',
-        'y',
-        'z',
-        'anchor_count',
-        'quality'
-    ])
-    df.rename(
-        columns = {
-            'timestamp_secondary': 'timestamp',
-            'serial_number': 'device_serial_number'
-        },
-        inplace=True
-    )
     df['x_meters'] = df['x']/1000.0
     df['y_meters'] = df['y']/1000.0
     df['z_meters'] = df['z']/1000.0
     df.drop(
-        columns=['x', 'y', 'z'],
+        columns=[
+            'type',
+            'battery_percentage',
+            'temperature',
+            'scale',
+            'x',
+            'y',
+            'z'
+        ],
         inplace=True
     )
-    df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
-    df.dropna(subset=['timestamp'], inplace=True)
-    df.set_index('timestamp', inplace=True)
-    df.sort_index(inplace=True)
     return df
 
+def fetch_cuwb_accelerometer_data(
+    environment_name,
+    start_time,
+    end_time,
+    read_chunk_size=2,
+    device_type='UWBTAG'
+):
+    df = fetch_cuwb_data(
+        environment_name,
+        start_time,
+        end_time,
+        read_chunk_size,
+        device_type
+    )
+    df = df.loc[df['type'] == 'accelerometer'].copy()
+    df.drop(
+        columns=[
+            'type',
+            'battery_percentage',
+            'temperature',
+            'anchor_count',
+            'quality',
+            'smoothing',
+
+        ],
+        inplace=True
+    )
+    return df
+
+def fetch_cuwb_status_data(
+    environment_name,
+    start_time,
+    end_time,
+    read_chunk_size=2,
+    device_type='UWBTAG'
+):
+    df = fetch_cuwb_data(
+        environment_name,
+        start_time,
+        end_time,
+        read_chunk_size,
+        device_type
+    )
+    df = df.loc[df['type'] == 'status'].copy()
+    df.drop(
+        columns=[
+            'type',
+            'x',
+            'y',
+            'z',
+            'scale',
+            'anchor_count',
+            'quality',
+            'smoothing',
+
+        ],
+        inplace=True
+    )
+    return df
 
 def fetch_cuwb_tag_entity_assignments(
     device_type='UWBTAG'
