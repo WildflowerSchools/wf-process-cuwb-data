27c7752299323a88a2298a4afc3c45c3e0f68e87
Theodore Quinn <tcquinn@tcquinn.org>
2020-01-18 09:28:32 -0600

    Add functions for fetching entity info and material names

--- process_cuwb_data/core.py
+++ process_cuwb_data/core.py
@@ -391,3 +391,70 @@ def fetch_material_assignments(
                         assignments_dict[tray_id][assignment_index - 1]['end']
                     ))
     return assignments_dict
+
+def fetch_entity_info():
+    client = MinimalHoneycombClient()
+    result = client.request(
+        request_type="query",
+        request_name='entityAssignments',
+        arguments=None,
+        return_object = [
+            {'data': [
+                'entity_assignment_id',
+                {'entity': [
+                    'entity_type: __typename',
+                    {'... on Tray': [
+                        'tray_id',
+                        'tray_name: name'
+                    ]},
+                    {'... on Person': [
+                        'person_id',
+                        'person_name: name',
+                        'person_short_name: short_name'
+                    ]}
+                ]}
+            ]}
+        ]
+    )
+    df = json_normalize(result.get('data'))
+    df.rename(
+        columns={
+            'entity.entity_type': 'entity_type',
+            'entity.tray_id': 'tray_id',
+            'entity.tray_name': 'tray_name',
+            'entity.person_id': 'person_id',
+            'entity.person_name': 'person_name',
+            'entity.person_short_name': 'person_short_name',
+        },
+        inplace=True
+    )
+    df.set_index('entity_assignment_id', inplace=True)
+    return df
+
+def fetch_material_names(
+):
+    client = MinimalHoneycombClient()
+    result = client.request(
+        request_type="query",
+        request_name='materialAssignments',
+        arguments=None,
+        return_object = [
+            {'data': [
+                'material_assignment_id',
+                {'material': [
+                    'material_id',
+                    'material_name: name'
+                ]}
+            ]}
+        ]
+    )
+    df = json_normalize(result.get('data'))
+    df.rename(
+        columns={
+            'material.material_id': 'material_id',
+            'material.material_name': 'material_name'
+        },
+        inplace=True
+    )
+    df.set_index('material_assignment_id', inplace=True)
+    return df
