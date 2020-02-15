a95b51e433d00be673bcdfbfcf2d4e30bbbe29f9
Theodore Quinn <tcquinn@tcquinn.org>
2020-02-15 14:23:46 -0800

    Add function to integrate ground truth data with features data

--- process_cuwb_data/features.py
+++ process_cuwb_data/features.py
@@ -235,3 +235,25 @@ def fetch_ground_truth_data(
     df['start_datetime'] = df['start_datetime'].dt.tz_localize(time_zone_name).dt.tz_convert('UTC')
     df['end_datetime'] = df['end_datetime'].dt.tz_localize(time_zone_name).dt.tz_convert('UTC')
     return df
+
+def add_ground_truth_data(
+    df_features,
+    df_ground_truth,
+    baseline_state='Not carried',
+    inplace=False
+):
+    if not inplace:
+        df_features = df_features.copy()
+    df_features['ground_truth_state'] = baseline_state
+    for index, row in df_ground_truth.iterrows():
+        if row['ground_truth_state'] != baseline_state:
+            df_features.loc[
+                (
+                    (df_features['device_id'] == row['device_id']) &
+                    (df_features.index >= row['start_datetime']) &
+                    (df_features.index <= row['end_datetime'])
+                ),
+                'ground_truth_state'
+            ] = row['ground_truth_state']
+    if not inplace:
+        return df_features
