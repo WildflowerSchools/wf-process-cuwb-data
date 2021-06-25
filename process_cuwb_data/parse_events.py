d7020436b03c293ead82f2f6ca431fcb8b44eeec
Theodore Quinn <tcquinn@tcquinn.org>
2021-06-25 13:04:12 -0700

    Add function to find best camera view for an event

--- process_cuwb_data/parse_events.py
+++ process_cuwb_data/parse_events.py
@@ -1,4 +1,8 @@
+import honeycomb_io
+import cv_utils
 import pandas as pd
+import numpy as np
+import datetime
 
 from process_cuwb_data.utils.log import logger
 
@@ -150,3 +154,137 @@ def describe_material_event(event, time_zone):
         description_text
     )
     return description
+
+def best_camera(
+    timestamp,
+    tray_device_id,
+    default_camera_device_id,
+    environment_id=None,
+    environment_name=None,
+    camera_dict=None,
+    camera_calibrations=None,
+    position_window_seconds=4,
+    imputed_z_position = 1.0,
+    chunk_size=100,
+    client=None,
+    uri=None,
+    token_uri=None,
+    audience=None,
+    client_id=None,
+    client_secret=None
+):
+    if camera_dict is None:
+        if environment_id is None and environment_name is None:
+            raise ValueError('If camera dictionary is not specified, must specify either environment ID or environment name')
+        camera_info = honeycomb_io.fetch_devices(
+            device_types=honeycomb_io.DEFAULT_CAMERA_DEVICE_TYPES,
+            environment_id=environment_id,
+            environment_name=environment_name,
+            start=timestamp,
+            end=timestamp,
+            output_format='dataframe',
+            chunk_size=chunk_size,
+            client=client,
+            uri=uri,
+            token_uri=token_uri,
+            audience=audience,
+            client_id=client_id,
+            client_secret=client_secret
+        )
+        camera_dict = camera_info['device_name'].to_dict()
+    camera_device_ids = list(camera_dict.keys())
+    if camera_calibrations is None:
+        camera_calibrations = honeycomb_io.fetch_camera_calibrations(
+            camera_ids=camera_device_ids,
+            start=timestamp,
+            end=timestamp,
+            chunk_size=chunk_size,
+            client=client,
+            uri=uri,
+            token_uri=token_uri,
+            audience=audience,
+            client_id=client_id,
+            client_secret=client_secret
+        )
+    position_window_start = timestamp - datetime.timedelta(seconds=position_window_seconds/2)
+    position_window_end = timestamp + datetime.timedelta(seconds=position_window_seconds/2)
+    position_data = honeycomb_io.fetch_cuwb_position_data(
+        start=position_window_start,
+        end=position_window_end,
+        device_ids=[tray_device_id],
+        environment_id=None,
+        environment_name=None,
+        device_types=['UWBTAG'],
+        output_format='dataframe',
+        sort_arguments=None,
+        chunk_size=1000,
+        client=client,
+        uri=uri,
+        token_uri=token_uri,
+        audience=audience,
+        client_id=client_id,
+        client_secret=client_secret
+    )
+    position = np.nanmedian(position_data.loc[:, ['x', 'y', 'z']].values, axis=0)
+    if imputed_z_position is not None:
+        position[2] = imputed_z_position
+    view_data_list = list()
+    for camera_device_id, camera_calibration in camera_calibrations.items():
+        camera_name = camera_dict.get(camera_device_id)
+        camera_position = cv_utils.extract_camera_position(
+            rotation_vector=camera_calibration['rotation_vector'],
+            translation_vector=camera_calibration['translation_vector']
+        )
+        distance_from_camera = np.linalg.norm(np.subtract(
+            position,
+            camera_position
+        ))
+        image_position = cv_utils.project_points(
+            object_points=position,
+            rotation_vector=camera_calibration['rotation_vector'],
+            translation_vector=camera_calibration['translation_vector'],
+            camera_matrix=camera_calibration['camera_matrix'],
+            distortion_coefficients=camera_calibration['distortion_coefficients'],
+            remove_behind_camera=True,
+            remove_outside_frame=True,
+            image_corners=np.asarray([
+                [0.0, 0.0],
+                [camera_calibration['image_width'], camera_calibration['image_height']]
+            ])
+        )
+        image_position = np.squeeze(image_position)
+        if np.all(np.isfinite(image_position)):
+            in_frame = True
+            distance_from_image_center = np.linalg.norm(np.subtract(
+                image_position,
+                [camera_calibration['image_width']/2, camera_calibration['image_height']/2]
+            ))
+            in_middle = (
+                image_position[0] > camera_calibration['image_width']*(1.0/10.0) and
+                image_position[0] < camera_calibration['image_width']*(9.0/10.0) and
+                image_position[1] > camera_calibration['image_height']*(1.0/10.0) and
+                image_position[1] < camera_calibration['image_height']*(9.0/10.0)
+            )
+        else:
+            in_frame=False
+            distance_from_image_center=None
+            in_middle=False
+        view_data_list.append({
+            'camera_device_id': camera_device_id,
+            'camera_name': camera_name,
+            'position': position,
+            'distance_from_camera': distance_from_camera,
+            'image_position': image_position,
+            'distance_from_image_center': distance_from_image_center,
+            'in_frame': in_frame,
+            'in_middle': in_middle,
+        })
+    view_data = pd.DataFrame(view_data_list).set_index('camera_device_id')
+    if not view_data['in_frame'].any():
+        best_camera_device_id = default_camera_device_id
+    elif not view_data['in_middle'].any():
+        best_camera_device_id = view_data.sort_values('distance_from_image_center').index[0]
+    else:
+        best_camera_device_id = view_data.loc[view_data['in_middle']].sort_values('distance_from_camera').index[0]
+    best_camera_name = camera_dict.get(best_camera_device_id)
+    return camera_calibrations, position_data, view_data, best_camera_device_id, best_camera_name
