26cf3b55a9563359349e46fbce80f4825ffcd234
Theodore Quinn <tcquinn@tcquinn.org>
2023-08-02 12:57:26 -0500

    Implement fetching of accelerometer data from RDS with caching

--- process_cuwb_data/rds_imu_data.py
+++ process_cuwb_data/rds_imu_data.py
@@ -100,6 +100,98 @@ def fetch_position_data(
         position_data.to_pickle(file_path)
     return position_data
 
+def fetch_accelerometer_data(
+    start,
+    end,
+    device_ids=None,
+    part_numbers=None,
+    serial_numbers=None,
+    tag_ids=None,
+    names=None,
+    environment_id=None,
+    environment_name=None,
+    include_device_info=False,
+    include_entity_info=False,
+    include_material_info=False,
+    use_cache=True,
+    cache_directory='/data/uwb_data',
+    rds_connection=None,
+    rds_dbname=None,
+    rds_user=None,
+    rds_password=None,
+    rds_host=None,
+    rds_port=None,
+    honeycomb_chunk_size=100,
+    honeycomb_client=None,
+    honeycomb_uri=None,
+    honeycomb_token_uri=None,
+    honeycomb_audience=None,
+    honeycomb_client_id=None,
+    honeycomb_client_secret=None,
+):
+    if use_cache:
+        file_path = generate_file_path(
+            filename_prefix='accelerometer_data',
+            start=start,
+            end=end,
+            device_ids=device_ids,
+            part_numbers=part_numbers,
+            serial_numbers=serial_numbers,
+            tag_ids=tag_ids,
+            names=names,
+            environment_id=environment_id,
+            environment_name=environment_name,
+            include_device_info=include_device_info,
+            include_entity_info=include_entity_info,
+            include_material_info=include_material_info,
+            cache_directory=cache_directory,
+            honeycomb_chunk_size=honeycomb_chunk_size,
+            honeycomb_client=honeycomb_client,
+            honeycomb_uri=honeycomb_uri,
+            honeycomb_token_uri=honeycomb_token_uri,
+            honeycomb_audience=honeycomb_audience,
+            honeycomb_client_id=honeycomb_client_id,
+            honeycomb_client_secret=honeycomb_client_secret,
+        )
+        if file_path.is_file():
+            accelerometer_data = pd.read_pickle(file_path)
+            logger.info(f'File {file_path} exists locally. Fetching from local')
+            return accelerometer_data
+    logger.info('Fetching data from RDS database')
+    client = honeycomb_rds_client.HoneycombRDSClient(
+        dbname=rds_dbname,
+        user=rds_user,
+        password=rds_password,
+        host=rds_host,
+        port=rds_port
+    )
+    accelerometer_data = client.fetch_accelerometer_data(
+        start=start,
+        end=end,
+        device_ids=device_ids,
+        part_numbers=part_numbers,
+        serial_numbers=serial_numbers,
+        tag_ids=tag_ids,
+        names=names,
+        environment_id=environment_id,
+        environment_name=environment_name,
+        include_device_info=include_device_info,
+        include_entity_info=include_entity_info,
+        include_material_info=include_material_info,
+        connection=rds_connection,
+        honeycomb_chunk_size=honeycomb_chunk_size,
+        honeycomb_client=honeycomb_client,
+        honeycomb_uri=honeycomb_uri,
+        honeycomb_token_uri=honeycomb_token_uri,
+        honeycomb_audience=honeycomb_audience,
+        honeycomb_client_id=honeycomb_client_id,
+        honeycomb_client_secret=honeycomb_client_secret,
+    )
+    if use_cache:
+        logger.info(f'Saving data locally as {file_path}')
+        accelerometer_data.to_pickle(file_path)
+    return accelerometer_data
+
 def generate_file_path(
     filename_prefix,
     start,
